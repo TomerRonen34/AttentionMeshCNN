fce71570c318f90e94d9478ff20704f9b6c17b27
tomershshsh <tomer.shanny@gmail.com>
2020-01-11 17:10:58 +0200

    add two implementations of all pairs shortest path. returns matrix of distances - now with the files :(

---
+++ lang_perf/cython_class.pyx
@@ -0,0 +1,41 @@
+from libcpp.vector cimport vector
+import numpy as np
+
+NOT_PROCESSED = -1
+
+cdef extern from "cpp_solver.h":
+    cdef cppclass CppSolver:
+        #Methods
+        CppSolver(vector[vector[int]]*) except +
+        void run_bfs(int, vector[int]*)
+        void compute_hash()
+
+
+cdef class Solver:
+    cdef vector[vector[int]] graph
+    cdef int num_edges
+    cdef CppSolver* cpp_solver
+
+    def init(self, adj_lists):
+        self.num_edges = len(adj_lists)
+
+        cdef vector[vector[int]] graph
+        graph.resize(self.num_edges)
+        for s, neighs in enumerate(adj_lists):
+            for t in neighs:
+                s = int(s)
+                t = int(t)
+
+                graph[s].push_back(t)
+                graph[t].push_back(s)
+
+        self.graph = graph
+        self.cpp_solver = new CppSolver(&self.graph)
+
+    def all_pairs_shortest_path(self):
+        cdef vector[vector[int]] distances
+        distances.resize(self.num_edges)
+        for v in range(self.num_edges):
+            self.cpp_solver.run_bfs(v, &distances[v])
+
+        return distances
